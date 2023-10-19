240efe98f1eac8f1225b8c78e69e350c9cd65ac4
regenerik <regenerik@gmail.com>
2023-10-19 02:17:17 +0000

    terminado todoList

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,47 +1,75 @@
 import React, { useState } from "react";
-import style from '../../styles/home.module.css'
+import style from '../../styles/home.module.css';
 
-
-//create your first component
 const Home = () => {
+  const [todo, setTodo] = useState("");
+  const [list, setList] = useState([]);
+  const [showX, setShowX] = useState(null);
 
-	const [todo, setTodo] = useState("")
-	const [list, setList] = useState([])
+  const modifToDo = (e) => {
+    setTodo(e.target.value);
+  };
 
-	const modifToDo = (e) => {
-		setTodo(e.target.value)
-	}
+  const addToList = (e) => {
+    if (e.key === "Enter" && todo.trim() !== "") {
+      const newTask = {
+        id: Date.now(),
+        text: todo,
+      };
+      setList([...list, newTask]);
+      setTodo("");
+    }
+  };
 
-	const addToList = (e) => {
-		if (e.key === "Enter" && todo !== "") {
-			setList([...list, todo])
-			console.log("La lista actualmente tiene: ", list)
-			setTodo("")
-		}
-	}
+  const removeTask = (taskId) => {
+    const updatedList = list.filter((task) => task.id !== taskId);
+    setList(updatedList);
+  };
 
-	return (
-		<div className={`text-center total ${style.total}`}>
-			<div>
-				<ul className={`list-group ${style.laLista}`}>
-				<input className={`list-group-item ${style.cadaTarea}`} value={todo} onChange={(e) => modifToDo(e)} onKeyDown={(e)=>addToList(e)}/>
-				{	
-					list.length > 0 ? (
-						<>
-							{list.map((ele,indx) =>(
-							<li key={indx} className={`list-group-item ${style.cadaTarea}`}>{ele}</li>
-							))}
-							<li className={`list-group-item ${style.textoDiferente}`}>{`Tienes ${list.length} tareas por hacer.`}</li>
-							
-						</>
-					):(
-						<li className={`list-group-item ${style.textoDiferente}`}>Nada para hacer! hora de netflix</li>
-					)
-				}
-				</ul>
-			</div>
-		</div>
-	);
+  return (
+    <div className={`text-center total ${style.total}`}>
+      <div>
+        <ul className={`list-group ${style.laLista}`}>
+          <input
+            className={`list-group-item ${style.inputTarea}`}
+            value={todo}
+            onChange={(e) => modifToDo(e)}
+            onKeyDown={(e) => addToList(e)}
+          />
+          {list.length > 0 ? (
+            <>
+              {list.map((task) => (
+                <div key={task.id}>
+                  <li
+                    className={`list-group-item ${style.cadaTarea}`}
+                    onMouseEnter={() => setShowX(task.id)}
+                    onMouseLeave={() => setShowX(null)}
+                  >
+                    {task.text}
+                    {showX === task.id && (
+                      <button
+                        onClick={() => removeTask(task.id)}
+                        className={style.deleteButton}
+                      >
+                        X
+                      </button>
+                    )}
+                  </li>
+                </div>
+              ))}
+              <li className={`list-group-item ${style.textoDiferente}`}>
+                {`Tienes ${list.length} tarea(s) por hacer.`}
+              </li>
+            </>
+          ) : (
+            <li className={`list-group-item ${style.textoDiferente}`}>
+              Nada para hacer, ¡hora de Netflix!
+            </li>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
